baf0195e61bcb9dbc3e585e496539fd774ccde39
MrHamburguesa <javierrojas14.jr@gmail.com>
2023-06-14 10:26:32 -0400

    Permite leer la base de datos completa

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -1,23 +1,75 @@
 # -*- coding: utf-8 -*-
-import click
+import hashlib
 import logging
+import glob
 from pathlib import Path
+
+import pandas as pd
+from datetime import datetime
+
+import click
 from dotenv import find_dotenv, load_dotenv
 
 
+COLS_A_OCUPAR = {
+    "Código Reserva Atención": str,
+    "Rut Paciente": str,
+    "Nombre Paciente": str,
+    "Apellido Paterno Paciente": str,
+    "Apellido Materno Paciente": str,
+    "Fecha Nacimiento": datetime,
+    "sexo": str,
+    "Fecha Reserva": datetime,
+    "Fecha Atención": datetime,
+    "Rut Profesional": str,
+    "Nombre Especialidad": str,
+    "Código Diagnóstico": str,
+    "Nombre Diagnóstico": str,
+    "Detalle Atención": str,
+    "Año": pd.Int16Dtype,
+}
+
+COLS_A_HASHEAR = [
+    "Rut Paciente",
+    "Nombre Paciente",
+    "Apellido Paterno Paciente",
+    "Apellido Materno Paciente",
+    "Rut Profesional",
+]
+
+
+def hashear_columna_texto(serie_texto):
+    serie_hasheada = serie_texto.copy()
+
+    serie_hasheada = serie_hasheada.astype(str).apply(
+        lambda x: hashlib.sha512(x.encode()).hexdigest()
+    )
+
+    return serie_hasheada
+
+
 @click.command()
-@click.argument('input_filepath', type=click.Path(exists=True))
-@click.argument('output_filepath', type=click.Path())
+@click.argument("input_filepath", type=click.Path(exists=True))
+@click.argument("output_filepath", type=click.Path())
 def main(input_filepath, output_filepath):
-    """ Runs data processing scripts to turn raw data from (../raw) into
-        cleaned data ready to be analyzed (saved in ../processed).
+    """Runs data processing scripts to turn raw data from (../raw) into
+    cleaned data ready to be analyzed (saved in ../processed).
     """
     logger = logging.getLogger(__name__)
-    logger.info('making final data set from raw data')
+    logger.info("making final data set from raw data")
+    df = pd.concat(
+        (
+            pd.read_excel(archivo, usecols=COLS_A_OCUPAR.keys())
+            for archivo in glob.glob(f"{input_filepath}/*.xlsx")
+        )
+    )
+
+    df.loc[:, COLS_A_HASHEAR] = df.loc[:, COLS_A_HASHEAR].apply(hashear_columna_texto)
+    df.to_csv(output_filepath, encoding="latin-1", index=False, sep=";", errors="replace")
 
 
-if __name__ == '__main__':
-    log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+if __name__ == "__main__":
+    log_fmt = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
     logging.basicConfig(level=logging.INFO, format=log_fmt)
 
     # not used in this stub but often useful for finding various files
