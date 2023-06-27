a69646664d5421e3ab20f354ce00e44c81caa929
MrHamburguesa <javierrojas14.jr@gmail.com>
2023-06-27 10:32:10 -0400

    Genera intervalos de edad

--- src/features/build_features.py
+++ src/features/build_features.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 
 
 def formatear_fechas_ambulatorio(df_procesada):
@@ -9,5 +10,22 @@ def formatear_fechas_ambulatorio(df_procesada):
     )
     tmp["Fecha Reserva"] = pd.to_datetime(tmp["Fecha Reserva"], yearfirst=True)
     tmp["Fecha Atención"] = pd.to_datetime(tmp["Fecha Atención"], yearfirst=True)
+
+    return tmp
+
+
+def agregar_rango_etario(df_procesada):
+    tmp = df_procesada.copy()
+
+    edad_primera_consulta = (tmp["Fecha Atención"] - tmp["Fecha Nacimiento"]) / np.timedelta64(
+        1, "Y"
+    )
+
+    tmp["EDAD_PRIMERA_CONSULTA"] = edad_primera_consulta
+    maxima_edad = int(round(max(edad_primera_consulta), 0))
+
+    tmp["RANGO_ETARIO_PRIMERA_CONSULTA"] = pd.cut(
+        edad_primera_consulta, range(0, maxima_edad + 1, 10)
+    )
 
     return tmp
