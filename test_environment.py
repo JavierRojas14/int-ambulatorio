843e77c5dcee907020e6d575827062848d6cfa96
MrHamburguesa <javierrojas14.jr@gmail.com>
2023-08-04 09:51:51 -0400

    Agrega nuevos tests de borde

--- test_environment.py
+++ test_environment.py
@@ -8,37 +8,59 @@ REQUIRED_PYTHON = "python3"
 
 
 class TestUnirFilasRepetidas(unittest.TestCase):
-
-    def test_unir_filas_repetidas(self):
-        # Test case 1: Simple example
-        data = {
-            'col1': [1, 1, 2, 2, 3],
-            'col2': ['a', 'b', 'c', 'd', 'e'],
-            'col3': ['x', 'y', 'z', 'w', 'v']
-        }
-        df = pd.DataFrame(data)
-        expected_result = pd.DataFrame({
-            'col1': [1, 2, 3],
-            'col2': ['a, b', 'c, d', 'e'],
-            'col3': ['x, y', 'z, w', 'v']
-        })
-        
-        actual_result = unir_filas_repetidas(df, ['col1'], 'col2')
+    def test_unir_filas_repetidas_empty_dataframe(self):
+        # Test case: Empty DataFrame
+        df = pd.DataFrame()
+        expected_result = pd.DataFrame()
+        actual_result = unir_filas_repetidas(df, ["col1"], "col2")
         pd.testing.assert_frame_equal(expected_result, actual_result)
 
-        # Test case 2: More complex example
+    def test_unir_filas_repetidas_no_duplicates(self):
+        # Test case: No duplicate values in columnas_repetidas
+        data = {"col1": [1, 2, 3], "col2": ["a", "b", "c"], "col3": ["x", "y", "z"]}
+        df = pd.DataFrame(data)
+        expected_result = df[["col1", "col2"]]
+        actual_result = unir_filas_repetidas(df, ["col1"], "col2")
+        pd.testing.assert_frame_equal(expected_result, actual_result)
+
+    def test_unir_filas_repetidas_single_distinct_value(self):
+        # Test case: Single distinct value in columna_distinta
+        data = {"col1": [1, 1, 2, 2], "col2": ["a", "b", "c", "d"], "col3": ["x", "x", "y", "y"]}
+        df = pd.DataFrame(data)
+        expected_result = pd.DataFrame({"col1": [1, 2], "col2": ["a, b", "c, d"]})
+        actual_result = unir_filas_repetidas(df, ["col1"], "col2")
+        pd.testing.assert_frame_equal(expected_result, actual_result)
+
+    def test_unir_filas_repetidas_multiple_distinct_values(self):
+        # Test case: Multiple distinct values in columna_distinta
         data = {
-            'col1': [1, 1, 1, 2, 2, 3],
-            'col2': ['a', 'b', 'c', 'd', 'e', 'f'],
-            'col3': ['x', 'y', 'z', 'w', 'v', 'u']
+            "col1": [1, 1, 2, 2, 3, 3],
+            "col2": ["a", "b", "c", "d", "e", "f"],
+            "col3": ["x", "y", "z", "w", "x", "y"],
         }
         df = pd.DataFrame(data)
-        expected_result = pd.DataFrame({
-            'col1': [1, 2, 3],
-            'col2': ['a, b, c', 'd, e', 'f'],
-            'col3': ['x, y, z', 'w, v', 'u']
-        })
-        actual_result = unir_filas_repetidas(df, ['col1'], 'col2')
+        expected_result = pd.DataFrame({"col1": [1, 2, 3], "col2": ["a, b", "c, d", "e, f"]})
+        actual_result = unir_filas_repetidas(df, ["col1"], "col2")
+        pd.testing.assert_frame_equal(expected_result, actual_result)
+
+    def test_unir_filas_repetidas_non_string_values(self):
+        # Test case: Non-string values in columna_distinta
+        data = {"col1": [1, 1, 2, 2], "col2": ["a", "b", "c", "d"], "col3": [10, 10, 20, 20]}
+        df = pd.DataFrame(data)
+        expected_result = pd.DataFrame({"col1": [1, 2], "col2": ["a, b", "c, d"]})
+        actual_result = unir_filas_repetidas(df, ["col1"], "col2")
+        pd.testing.assert_frame_equal(expected_result, actual_result)
+
+    def test_unir_filas_repetidas_missing_values(self):
+        # Test case: Handling missing values (NaN)
+        data = {
+            "col1": [1, 1, 2, 2, None, None],
+            "col2": ["a", "b", "c", "d", "e", "f"],
+            "col3": ["x", "x", "y", "y", "z", "z"],
+        }
+        df = pd.DataFrame(data)
+        expected_result = pd.DataFrame({"col1": [1, 2], "col2": ["a, b", "c, d"]})
+        actual_result = unir_filas_repetidas(df, ["col1"], "col2")
         pd.testing.assert_frame_equal(expected_result, actual_result)
 
 
@@ -49,18 +71,16 @@ def main():
     elif REQUIRED_PYTHON == "python3":
         required_major = 3
     else:
-        raise ValueError("Unrecognized python interpreter: {}".format(
-            REQUIRED_PYTHON))
+        raise ValueError("Unrecognized python interpreter: {}".format(REQUIRED_PYTHON))
 
     if system_major != required_major:
         raise TypeError(
-            "This project requires Python {}. Found: Python {}".format(
-                required_major, sys.version))
+            "This project requires Python {}. Found: Python {}".format(required_major, sys.version)
+        )
     else:
         print(">>> Development environment passes all tests!")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
     main()
-    
