2007c9ec6e94ff34677ad611cc45551d9c272be5
MrHamburguesa <javierrojas14.jr@gmail.com>
2023-08-04 09:33:24 -0400

    Escribe unidad de test y falla

--- test_environment.py
+++ test_environment.py
@@ -1,6 +1,45 @@
 import sys
+import unittest
+import pandas as pd
+
+from src.data.make_dataset import unir_filas_repetidas
 
 REQUIRED_PYTHON = "python3"
+
+
+class TestUnirFilasRepetidas(unittest.TestCase):
+
+    def test_unir_filas_repetidas(self):
+        # Test case 1: Simple example
+        data = {
+            'col1': [1, 1, 2, 2, 3],
+            'col2': ['a', 'b', 'c', 'd', 'e'],
+            'col3': ['x', 'y', 'z', 'w', 'v']
+        }
+        df = pd.DataFrame(data)
+        expected_result = pd.DataFrame({
+            'col1': [1, 2, 3],
+            'col2': ['a, b', 'c, d', 'e'],
+            'col3': ['x, y', 'z, w', 'v']
+        })
+        
+        actual_result = unir_filas_repetidas(df, ['col1'], 'col2')
+        pd.testing.assert_frame_equal(expected_result, actual_result)
+
+        # Test case 2: More complex example
+        data = {
+            'col1': [1, 1, 1, 2, 2, 3],
+            'col2': ['a', 'b', 'c', 'd', 'e', 'f'],
+            'col3': ['x', 'y', 'z', 'w', 'v', 'u']
+        }
+        df = pd.DataFrame(data)
+        expected_result = pd.DataFrame({
+            'col1': [1, 2, 3],
+            'col2': ['a, b, c', 'd, e', 'f'],
+            'col3': ['x, y, z', 'w, v', 'u']
+        })
+        actual_result = unir_filas_repetidas(df, ['col1'], 'col2')
+        pd.testing.assert_frame_equal(expected_result, actual_result)
 
 
 def main():
@@ -22,4 +61,6 @@ def main():
 
 
 if __name__ == '__main__':
+    unittest.main()
     main()
+    
