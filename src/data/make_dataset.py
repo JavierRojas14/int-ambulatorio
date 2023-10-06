b0fde3c09736f38f7332dea3907101eed635890a
MrHamburguesa <javierrojas14.jr@gmail.com>
2023-10-06 14:53:44 -0300

    Exporta base de HIS y Datos est

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -357,24 +357,24 @@ def main(input_filepath, output_filepath):
     logger = logging.getLogger(__name__)
     logger.info("making final data set from raw data")
 
-    # df_diagnosticos = leer_y_preprocesar_ambulatorio_diagnosticos(input_filepath)
-    # df_procedimientos = leer_y_preprocesar_ambulatorio_procedimientos(input_filepath)
+    df_diagnosticos = leer_y_preprocesar_ambulatorio_diagnosticos(input_filepath)
+    df_procedimientos = leer_y_preprocesar_ambulatorio_procedimientos(input_filepath)
     df_track = leer_y_preprocesar_ambulatorio_trackcare(input_filepath)
 
-    # df_diagnosticos.to_csv(
-    #     f"{output_filepath}/datos_limpios_diagnosticos.csv",
-    #     encoding="latin-1",
-    #     index=False,
-    #     sep=";",
-    #     errors="replace",
-    # )
-    # df_procedimientos.to_csv(
-    #     f"{output_filepath}/datos_limpios_procedimientos.csv",
-    #     encoding="latin-1",
-    #     index=False,
-    #     sep=";",
-    #     errors="replace",
-    # )
+    df_diagnosticos.to_csv(
+        f"{output_filepath}/datos_limpios_diagnosticos.csv",
+        encoding="latin-1",
+        index=False,
+        sep=";",
+        errors="replace",
+    )
+    df_procedimientos.to_csv(
+        f"{output_filepath}/datos_limpios_procedimientos.csv",
+        encoding="latin-1",
+        index=False,
+        sep=";",
+        errors="replace",
+    )
 
     df_track.to_csv(
         f"{output_filepath}/datos_limpios_track.csv",
