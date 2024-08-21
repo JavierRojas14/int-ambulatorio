d5e27be6b382a3544ce4081133788f35be65e8e8
Javier Rojas <javierrojas14.jr@gmail.com>
2024-08-21 12:23:30 -0400

    Renombra variable de his

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -21,11 +21,11 @@ def main(input_filepath, output_filepath):
     logger = logging.getLogger(__name__)
     logger.info("making final data set from raw data")
 
-    df_diagnosticos = leer_his(input_filepath)
+    df_his = leer_his(input_filepath)
     # df_procedimientos = leer_y_preprocesar_ambulatorio_procedimientos(input_filepath)
     # df_track = leer_y_preprocesar_ambulatorio_trackcare(input_filepath)
 
-    df_diagnosticos.to_csv(f"{output_filepath}/his_procesada.csv")
+    df_his.to_csv(f"{output_filepath}/his_procesada.csv")
     # df_procedimientos.to_csv(
     #     f"{output_filepath}/datos_limpios_procedimientos.csv",
     #     encoding="latin-1",
